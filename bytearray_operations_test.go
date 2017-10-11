502b8395abe689580a871ad5f829b08ade8d2e0c
Romain Lespinasse <romain.lespinasse@gmail.com>
2017-10-12 01:50:55 +0200

    [#3] Add extract function for a subset of a byte array (#22)
    - RightShift learn to support shift greater than a byte size
    - LeftShift learn to support shift greater than a byte size
    - ExtractBytes is added
    - ComputeSize is added
    - Trim is added
    
    Closes #3

--- bytearray_operations_test.go
+++ bytearray_operations_test.go
@@ -12,9 +12,65 @@ func TestRightShift(t *testing.T) {
 	}
 }
 
+func TestRightShiftOver1Byte(t *testing.T) {
+	var result = RightShift([]byte{0x99, 0xBA}, 9)
+	if !reflect.DeepEqual(result, []byte{0x74, 0x00}) {
+		t.Errorf("RightShiftOver1Byte don't work: %x", result)
+	}
+}
+
 func TestLeftShift(t *testing.T) {
 	var result = LeftShift([]byte{0x99, 0xBA}, 1)
 	if !reflect.DeepEqual(result, []byte{0x4C, 0xDD}) {
 		t.Errorf("LeftShift don't work: %x", result)
+	}
+}
+
+func TestLeftShiftOver1Byte(t *testing.T) {
+	var result = LeftShift([]byte{0x99, 0xBA}, 9)
+	if !reflect.DeepEqual(result, []byte{0x00, 0x4C}) {
+		t.Errorf("LeftShiftOver1Byte don't work: %x", result)
+	}
+}
+
+func TestExtract1ByteFrom2Bytes(t *testing.T) {
+	var result = ExtractBytes([]byte{0x99, 0xBA}, 7, 8)
+	if !reflect.DeepEqual(result, []byte{0x03}) {
+		t.Errorf("Extract1ByteFrom2Bytes don't work: %x", result)
+	}
+}
+
+func TestExtract1ByteFrom3Bytes(t *testing.T) {
+	var result = ExtractBytes([]byte{0x99, 0xBA, 0xDE}, 15, 16)
+	if !reflect.DeepEqual(result, []byte{0x03}) {
+		t.Errorf("Extract1ByteFrom3Bytes don't work: %x", result)
+	}
+}
+
+func TestExtract2ByteFrom3Bytes(t *testing.T) {
+	var result = ExtractBytes([]byte{0x99, 0xBA, 0xDE}, 8, 19)
+	if !reflect.DeepEqual(result, []byte{0x09, 0xBA}) {
+		t.Errorf("Extract2ByteFrom3Bytes don't work: %x", result)
+	}
+}
+
+func TestExtractAllBytes(t *testing.T) {
+	var result = ExtractBytes([]byte{0x99, 0xBA, 0xDE}, 0, 23)
+	if !reflect.DeepEqual(result, []byte{0x99, 0xBA, 0xDE}) {
+		t.Errorf("ExtractAllBytes don't work: %x", result)
+	}
+}
+
+func TestComputeSize(t *testing.T) {
+	var result = ComputeSize(8, 75)
+	if result != 9 {
+		t.Errorf("ComputeSize don't work: %d", result)
+	}
+}
+
+func TestTrim(t *testing.T) {
+	var result = Trim([]byte{0x99, 0xBA, 0x00, 0x02}, 2)
+	if !reflect.DeepEqual(result, []byte{0x00, 0x02}) {
+		t.Errorf("TestTrim don't work: %x", result)
 	}
 }
