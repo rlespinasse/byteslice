01b43a0c79cd3c0742789340ac6463ba2a260e0f
Romain Lespinasse <romain.lespinasse@gmail.com>
2017-10-11 20:29:46 +0200

    [#16] Use better parameter names (#17)
    Closes #16

--- bit_methods.go
+++ bit_methods.go
@@ -1,21 +1,21 @@
 package gobits
 
-// ContainsBit test if a bit (position b) is contains in a
-func ContainsBit(a byte, b uint8) bool {
-	return ContainsBits(a, SetBit(0x00, b))
+// ContainsBit test if a bit is contains in the data
+func ContainsBit(data byte, bit uint8) bool {
+	return ContainsBits(data, SetBit(0x00, bit))
 }
 
-// SetBit change the bit (b) of a to 1
-func SetBit(a byte, b uint8) byte {
-	return SetBits(a, 0x01<<b)
+// SetBit change the bit of the data to 1
+func SetBit(data byte, bit uint8) byte {
+	return SetBits(data, 0x01<<bit)
 }
 
-// UnsetBit change the bit (b) of a to 0
-func UnsetBit(a byte, b uint8) byte {
-	return UnsetBits(a, 0x01<<b)
+// UnsetBit change the bit of the data to 0
+func UnsetBit(data byte, bit uint8) byte {
+	return UnsetBits(data, 0x01<<bit)
 }
 
-// GetBit get the bit from a byte
-func GetBit(a byte, b uint8) byte {
-	return (a >> b & 0x01) << b
+// GetBit get the bit value from the data
+func GetBit(data byte, bit uint8) byte {
+	return (data >> bit & 0x01) << bit
 }
