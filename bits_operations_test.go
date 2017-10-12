5d20becf4108cf8bed11771ab208aa9ff085534f
Kurt Sussman <kls@merlot.com>
2017-10-11 19:15:17 -0700

    Switch to table-style tests for -Bit and -Bits functions
    Exercise happy path and sad path wherever possible.

--- bits_operations_test.go
+++ bits_operations_test.go
@@ -5,29 +5,95 @@ import (
 )
 
 func TestContainsBits(t *testing.T) {
-	var result = ContainsBits(0xF0, 0xA0)
-	if !result {
-		t.Errorf("ContainsBits don't work")
+	var val bool
+	tests := []struct {
+		name   string
+		a, b   byte
+		result bool
+	}{
+		{"low in hi", 0xf0, 5, false},
+		{"hi in hi", 0xf0, 0x20, true},
+		{"split", 0xf0, 0x22, false},
+	}
+	for _, tt := range tests {
+
+		val = ContainsBits(tt.a, tt.b)
+		if val != tt.result {
+			t.Errorf("Test '%v' failed: ContainsBits(%v, %v) was %v, should be %v",
+				tt.name,
+				tt.a, tt.b,
+				val,
+				tt.result)
+		}
 	}
 }
 
 func TestSetBits(t *testing.T) {
-	var result = SetBits(0xF0, 0x01)
-	if result != 0xF1 {
-		t.Errorf("SetBits don't work: %08b", result)
+	var val byte
+	tests := []struct {
+		name   string
+		a, b   byte
+		result byte
+	}{
+		{"set low bit", 0xf0, 0, 0xf0},
+		{"high and low", 0xf0, 0x11, 0xf1},
+	}
+	for _, tt := range tests {
+
+		val = SetBits(tt.a, tt.b)
+		if val != tt.result {
+			t.Errorf("Test '%v' failed: SetBits(%v, %v) was %v, should be %v",
+				tt.name,
+				tt.a, tt.b,
+				val,
+				tt.result)
+		}
 	}
 }
 
 func TestUnsetBits(t *testing.T) {
-	var result = UnsetBits(0xF1, 0x01)
-	if result != 0xF0 {
-		t.Errorf("UnsetBits don't work: %08b", result)
+	var val byte
+	tests := []struct {
+		name   string
+		a, b   byte
+		result byte
+	}{
+		{"unset zero bit", 0xf0, 7, 0xf0},
+		{"unset some bits", 0xf0, 0x11, 0xe0},
+	}
+	for _, tt := range tests {
+
+		val = UnsetBits(tt.a, tt.b)
+		if val != tt.result {
+			t.Errorf("Test '%v' failed: UnsetBits(%v, %v) was %v, should be %v",
+				tt.name,
+				tt.a, tt.b,
+				val,
+				tt.result)
+		}
 	}
 }
 
 func TestExtractBits(t *testing.T) {
-	var result = ExtractBits(0xDA, 3, 4)
-	if result != 0x03 {
-		t.Errorf("ExtractBits don't work: %08b", result)
+	var val byte
+	tests := []struct {
+		name    string
+		a, b, c byte
+		result  byte
+	}{
+		{"get bottom 3 bits", 0xf2, 0, 2, 2},
+		{"get low bit", 0xf0, 0, 0, 0},
+		{"lsb and msb out of order", 0xf0, 2, 0, 0},
+	}
+	for _, tt := range tests {
+
+		val = ExtractBits(tt.a, tt.b, tt.c)
+		if val != tt.result {
+			t.Errorf("Test '%v' failed: ExtractBits(%v, %v, %v) was %v, should be %v",
+				tt.name,
+				tt.a, tt.b, tt.c,
+				val,
+				tt.result)
+		}
 	}
 }
