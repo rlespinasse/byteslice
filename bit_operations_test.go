8c1474d8c259cb578e47ea276bccd1512905b704
Romain Lespinasse <romain.lespinasse@gmail.com>
2017-10-15 23:09:10 +0200

    [#26] Change tests/benchmarks to use subtests (#27)
    - Use subtests for tests
    - Use subtests for benchmarks
    - Use same input (Table-driven) for both
    - Run tests and benchmark during Travis builds
    
    Closes #26

--- bit_operations_test.go
+++ bit_operations_test.go
@@ -1,147 +1,149 @@
 package gobits
 
-import "testing"
+import (
+	"testing"
+)
+
+var testcasesContainsBit = []struct {
+	name   string
+	data   byte
+	bit    uint8
+	result bool
+}{
+	{"hi in hi", 0xf0, 5, true},
+	{"lo in hi", 0xf0, 2, false},
+}
 
 func TestContainsBit(t *testing.T) {
 	var val bool
-	tests := []struct {
-		name   string
-		data   byte
-		bit    uint8
-		result bool
-	}{
-		{"hi in hi", 0xf0, 5, true},
-		{"lo in hi", 0xf0, 2, false},
-	}
-	for _, tt := range tests {
-
-		val = ContainsBit(tt.data, tt.bit)
-		if val != tt.result {
-			t.Errorf("Test '%v' failed: ContainsBit(0x%x, %v) was %v, should be %v",
-				tt.name,
-				tt.data, tt.bit,
-				val,
-				tt.result)
-		}
+	for _, tc := range testcasesContainsBit {
+		t.Run(tc.name, func(t *testing.T) {
+			val = ContainsBit(tc.data, tc.bit)
+			if val != tc.result {
+				t.Errorf("ContainsBit(%x, %v) was %v, should be %v",
+					tc.data, tc.bit,
+					val,
+					tc.result)
+			}
+		})
 	}
 }
 
-func BenchmarkContainsBitTrue(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		ContainsBits(0xf0, 5)
+func BenchmarkContainsBit(b *testing.B) {
+	var val bool
+	for _, tc := range testcasesContainsBit {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				val = ContainsBit(tc.data, tc.bit)
+			}
+		})
 	}
 }
 
-func BenchmarkContainsBitFalse(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		ContainsBits(0xf0, 2)
-	}
+var testcasesSetBit = []struct {
+	name   string
+	data   byte
+	bit    uint8
+	result byte
+}{
+	{"set low bit", 0xf0, 0, 0xf1},
+	{"set already-set bit", 0xf0, 5, 0xf0},
 }
 
 func TestSetBit(t *testing.T) {
 	var val byte
-	tests := []struct {
-		name   string
-		data   byte
-		bit    uint8
-		result byte
-	}{
-		{"set low bit", 0xf0, 0, 0xf1},
-		{"set already-set bit", 0xf0, 5, 0xf0},
-	}
-	for _, tt := range tests {
-
-		val = SetBit(tt.data, tt.bit)
-		if val != tt.result {
-			t.Errorf("Test '%v' failed: SetBit(0x%x, %v) was 0x%x, should be 0x%x",
-				tt.name,
-				tt.data, tt.bit,
-				val,
-				tt.result)
-		}
+	for _, tc := range testcasesSetBit {
+		t.Run(tc.name, func(t *testing.T) {
+			val = SetBit(tc.data, tc.bit)
+			if val != tc.result {
+				t.Errorf("SetBit(%x, %v) was %x, should be %x",
+					tc.data, tc.bit,
+					val,
+					tc.result)
+			}
+		})
 	}
 }
 
-func BenchmarkSetBitLow(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		SetBit(0xf0, 0)
+func BenchmarkSetBit(b *testing.B) {
+	var val byte
+	for _, tc := range testcasesSetBit {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				val = SetBit(tc.data, tc.bit)
+			}
+		})
 	}
 }
 
-func BenchmarkSetBitAlreadySet(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		SetBit(0xf0, 5)
-	}
+var testcasesUnsetBit = []struct {
+	name   string
+	data   byte
+	bit    uint8
+	result byte
+}{
+	{"unset high bit", 0xf0, 7, 0x70},
+	{"unset already-clear bit", 0xf0, 0, 0xf0},
 }
 
 func TestUnsetBit(t *testing.T) {
 	var val byte
-	tests := []struct {
-		name   string
-		data   byte
-		bit    uint8
-		result byte
-	}{
-		{"unset high bit", 0xf0, 7, 0x70},
-		{"unset already-clear bit", 0xf0, 0, 0xf0},
-	}
-	for _, tt := range tests {
-
-		val = UnsetBit(tt.data, tt.bit)
-		if val != tt.result {
-			t.Errorf("Test '%v' failed: UnsetBit(0x%x, %v) was 0x%x, should be 0x%x",
-				tt.name,
-				tt.data, tt.bit,
-				val,
-				tt.result)
-		}
+	for _, tc := range testcasesUnsetBit {
+		t.Run(tc.name, func(t *testing.T) {
+			val = UnsetBit(tc.data, tc.bit)
+			if val != tc.result {
+				t.Errorf("UnsetBit(%x, %v) was %x, should be %x",
+					tc.data, tc.bit,
+					val,
+					tc.result)
+			}
+		})
 	}
 }
 
-func BenchmarkUnsetBitHigh(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		UnsetBit(0xf0, 7)
+func BenchmarkUnsetBit(b *testing.B) {
+	var val byte
+	for _, tc := range testcasesUnsetBit {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				val = UnsetBit(tc.data, tc.bit)
+			}
+		})
 	}
 }
 
-func BenchmarkUnsetBitAlreadyClear(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		UnsetBit(0xf0, 0)
-	}
+var testcasesGetBit = []struct {
+	name   string
+	data   byte
+	bit    uint8
+	result byte
+}{
+	{"get low bit of high nibble", 0xf0, 4, 0x10},
+	{"get low bit", 0xf0, 0, 0},
 }
 
 func TestGetBit(t *testing.T) {
 	var val byte
-	tests := []struct {
-		name   string
-		data   byte
-		bit    uint8
-		result byte
-	}{
-		{"get low bit of high nibble", 0xf0, 4, 0x10},
-		{"get low bit", 0xf0, 0, 0},
-	}
-	for _, tt := range tests {
-
-		val = GetBit(tt.data, tt.bit)
-		if val != tt.result {
-			t.Errorf("Test '%v' failed: GetBit(0x%x, %v) was 0x%x, should be 0x%x",
-				tt.name,
-				tt.data, tt.bit,
-				val,
-				tt.result)
-		}
+	for _, tc := range testcasesGetBit {
+		t.Run(tc.name, func(t *testing.T) {
+			val = GetBit(tc.data, tc.bit)
+			if val != tc.result {
+				t.Errorf("GetBit(%x, %v) was %x, should be %x",
+					tc.data, tc.bit,
+					val,
+					tc.result)
+			}
+		})
 	}
 }
 
-func BenchmarkGetBitHighNible(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		GetBit(0xf0, 4)
-	}
-}
-
-func BenchmarkGetBit(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		GetBit(0xf0, 0)
+func BenchmarkGetBit(b *testing.B) {
+	var val byte
+	for _, tc := range testcasesGetBit {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				val = GetBit(tc.data, tc.bit)
+			}
+		})
 	}
 }
--- bits_operations_test.go
+++ bits_operations_test.go
@@ -2,157 +2,145 @@ package gobits
 
 import "testing"
 
+var testcasesContainsBits = []struct {
+	name       string
+	data, bits byte
+	result     bool
+}{
+	{"low in hi", 0xf0, 0x05, false},
+	{"hi in hi", 0xf0, 0x20, true},
+	{"split", 0xf0, 0x22, false},
+}
+
 func TestContainsBits(t *testing.T) {
 	var val bool
-	tests := []struct {
-		name       string
-		data, bits byte
-		result     bool
-	}{
-		{"low in hi", 0xf0, 0x05, false},
-		{"hi in hi", 0xf0, 0x20, true},
-		{"split", 0xf0, 0x22, false},
-	}
-	for _, tt := range tests {
-
-		val = ContainsBits(tt.data, tt.bits)
-		if val != tt.result {
-			t.Errorf("Test '%v' failed: ContainsBits(0x%x, 0x%x) was %v, should be %v",
-				tt.name,
-				tt.data, tt.bits,
-				val,
-				tt.result)
-		}
+	for _, tc := range testcasesContainsBits {
+		t.Run(tc.name, func(t *testing.T) {
+			val = ContainsBits(tc.data, tc.bits)
+			if val != tc.result {
+				t.Errorf("ContainsBits(%x, %x) was %v, should be %v",
+					tc.data, tc.bits,
+					val,
+					tc.result)
+			}
+		})
 	}
 }
 
-func BenchmarkContainsBitsTrue(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		ContainsBits(0xf0, 0x20)
+func BenchmarkContainsBits(b *testing.B) {
+	var val bool
+	for _, tc := range testcasesContainsBits {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				val = ContainsBits(tc.data, tc.bits)
+			}
+		})
 	}
 }
 
-func BenchmarkContainsBitsFalse(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		ContainsBits(0xf0, 5)
-	}
-}
-
-func BenchmarkContainsBitsFalseSplit(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		ContainsBits(0xf0, 0x22)
-	}
+var testcasesSetBits = []struct {
+	name       string
+	data, bits byte
+	result     byte
+}{
+	{"set low bit", 0xf0, 0x00, 0xf0},
+	{"high and low", 0xf0, 0x11, 0xf1},
 }
 
 func TestSetBits(t *testing.T) {
 	var val byte
-	tests := []struct {
-		name       string
-		data, bits byte
-		result     byte
-	}{
-		{"set low bit", 0xf0, 0x00, 0xf0},
-		{"high and low", 0xf0, 0x11, 0xf1},
-	}
-	for _, tt := range tests {
-
-		val = SetBits(tt.data, tt.bits)
-		if val != tt.result {
-			t.Errorf("Test '%v' failed: SetBits(0x%x, 0x%x) was 0x%x, should be 0x%x",
-				tt.name,
-				tt.data, tt.bits,
-				val,
-				tt.result)
-		}
+	for _, tc := range testcasesSetBits {
+		t.Run(tc.name, func(t *testing.T) {
+			val = SetBits(tc.data, tc.bits)
+			if val != tc.result {
+				t.Errorf("SetBits(%x, %x) was %x, should be %x",
+					tc.data, tc.bits,
+					val,
+					tc.result)
+			}
+		})
 	}
 }
 
-func BenchmarkSetBitsLow(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		SetBits(0xf0, 0)
+func BenchmarkSetBits(b *testing.B) {
+	var val byte
+	for _, tc := range testcasesSetBits {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				val = SetBits(tc.data, tc.bits)
+			}
+		})
 	}
 }
 
-func BenchmarkSetBitsHighLow(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		SetBits(0xf0, 0x11)
-	}
+var testcasesUnsetBits = []struct {
+	name       string
+	data, bits byte
+	result     byte
+}{
+	{"unset zero bit", 0xf0, 0x07, 0xf0},
+	{"unset some bits", 0xf0, 0x11, 0xe0},
 }
 
 func TestUnsetBits(t *testing.T) {
 	var val byte
-	tests := []struct {
-		name       string
-		data, bits byte
-		result     byte
-	}{
-		{"unset zero bit", 0xf0, 0x07, 0xf0},
-		{"unset some bits", 0xf0, 0x11, 0xe0},
-	}
-	for _, tt := range tests {
-
-		val = UnsetBits(tt.data, tt.bits)
-		if val != tt.result {
-			t.Errorf("Test '%v' failed: UnsetBits(0x%x, 0x%x) was 0x%x, should be 0x%x",
-				tt.name,
-				tt.data, tt.bits,
-				val,
-				tt.result)
-		}
+	for _, tc := range testcasesUnsetBits {
+		t.Run(tc.name, func(t *testing.T) {
+			val = UnsetBits(tc.data, tc.bits)
+			if val != tc.result {
+				t.Errorf("UnsetBits(%x, %x) was %x, should be %x",
+					tc.data, tc.bits,
+					val,
+					tc.result)
+			}
+		})
 	}
 }
 
-func BenchmarkUnsetBitsZeroBits(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		UnsetBits(0xf0, 7)
+func BenchmarkUnsetBits(b *testing.B) {
+	var val byte
+	for _, tc := range testcasesUnsetBits {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				val = UnsetBits(tc.data, tc.bits)
+			}
+		})
 	}
 }
 
-func BenchmarkUnsetBitsSomeBits(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		UnsetBits(0xf0, 0x11)
-	}
+var testcasesExtractBits = []struct {
+	name                     string
+	data                     byte
+	lsbPosition, msbPosition uint8
+	result                   byte
+}{
+	{"get bottom 3 bits", 0xf2, 0, 2, 0x02},
+	{"get low bit", 0xf0, 0, 0, 0x00},
+	{"lsb and msb out of order", 0xf0, 2, 0, 0x00},
 }
 
 func TestExtractBits(t *testing.T) {
 	var val byte
-	tests := []struct {
-		name                     string
-		data                     byte
-		lsbPosition, msbPosition uint8
-		result                   byte
-	}{
-		{"get bottom 3 bits", 0xf2, 0, 2, 0x02},
-		{"get low bit", 0xf0, 0, 0, 0x00},
-		{"lsb and msb out of order", 0xf0, 2, 0, 0x00},
-	}
-	for _, tt := range tests {
-
-		val = ExtractBits(tt.data, tt.lsbPosition, tt.msbPosition)
-		if val != tt.result {
-			t.Errorf("Test '%v' failed: ExtractBits(0x%x, %v, %v) was 0x%x, should be 0x%x",
-				tt.name,
-				tt.data, tt.lsbPosition, tt.msbPosition,
-				val,
-				tt.result)
-		}
+	for _, tc := range testcasesExtractBits {
+		t.Run(tc.name, func(t *testing.T) {
+			val = ExtractBits(tc.data, tc.lsbPosition, tc.msbPosition)
+			if val != tc.result {
+				t.Errorf("ExtractBits(%x, %x) was %x, should be %x",
+					tc.data, tc.lsbPosition, tc.msbPosition,
+					val,
+					tc.result)
+			}
+		})
 	}
 }
 
-func BenchmarkExtractBitsBottom(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		ExtractBits(0xf0, 0, 2)
-	}
-}
-
-func BenchmarkExtractBitsLow(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		ExtractBits(0xf0, 0, 0)
-	}
-}
-
-func BenchmarkExtractBitsOutOfOrder(t *testing.B) {
-	for i := 0; i < t.N; i++ {
-		ExtractBits(0xf0, 2, 0)
+func BenchmarkExtractBits(b *testing.B) {
+	var val byte
+	for _, tc := range testcasesExtractBits {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				val = ExtractBits(tc.data, tc.lsbPosition, tc.msbPosition)
+			}
+		})
 	}
 }
