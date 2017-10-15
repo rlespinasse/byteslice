46cf7fb5fcff183c316a757d708d32fb07fb93b1
Romain Lespinasse <romain.lespinasse@gmail.com>
2017-10-15 15:43:36 +0200

    [#20] Change variables name in test functions to be compliant

--- bits_operations_test.go
+++ bits_operations_test.go
@@ -7,21 +7,21 @@ import (
 func TestContainsBits(t *testing.T) {
 	var val bool
 	tests := []struct {
-		name   string
-		a, b   byte
-		result bool
+		name       string
+		data, bits byte
+		result     bool
 	}{
-		{"low in hi", 0xf0, 5, false},
+		{"low in hi", 0xf0, 0x05, false},
 		{"hi in hi", 0xf0, 0x20, true},
 		{"split", 0xf0, 0x22, false},
 	}
 	for _, tt := range tests {
 
-		val = ContainsBits(tt.a, tt.b)
+		val = ContainsBits(tt.data, tt.bits)
 		if val != tt.result {
 			t.Errorf("Test '%v' failed: ContainsBits(%v, %v) was %v, should be %v",
 				tt.name,
-				tt.a, tt.b,
+				tt.data, tt.bits,
 				val,
 				tt.result)
 		}
@@ -31,20 +31,20 @@ func TestContainsBits(t *testing.T) {
 func TestSetBits(t *testing.T) {
 	var val byte
 	tests := []struct {
-		name   string
-		a, b   byte
-		result byte
+		name       string
+		data, bits byte
+		result     byte
 	}{
-		{"set low bit", 0xf0, 0, 0xf0},
+		{"set low bit", 0xf0, 0x00, 0xf0},
 		{"high and low", 0xf0, 0x11, 0xf1},
 	}
 	for _, tt := range tests {
 
-		val = SetBits(tt.a, tt.b)
+		val = SetBits(tt.data, tt.bits)
 		if val != tt.result {
 			t.Errorf("Test '%v' failed: SetBits(%v, %v) was %v, should be %v",
 				tt.name,
-				tt.a, tt.b,
+				tt.data, tt.bits,
 				val,
 				tt.result)
 		}
@@ -54,20 +54,20 @@ func TestSetBits(t *testing.T) {
 func TestUnsetBits(t *testing.T) {
 	var val byte
 	tests := []struct {
-		name   string
-		a, b   byte
-		result byte
+		name       string
+		data, bits byte
+		result     byte
 	}{
 		{"unset zero bit", 0xf0, 7, 0xf0},
 		{"unset some bits", 0xf0, 0x11, 0xe0},
 	}
 	for _, tt := range tests {
 
-		val = UnsetBits(tt.a, tt.b)
+		val = UnsetBits(tt.data, tt.bits)
 		if val != tt.result {
 			t.Errorf("Test '%v' failed: UnsetBits(%v, %v) was %v, should be %v",
 				tt.name,
-				tt.a, tt.b,
+				tt.data, tt.bits,
 				val,
 				tt.result)
 		}
@@ -77,9 +77,10 @@ func TestUnsetBits(t *testing.T) {
 func TestExtractBits(t *testing.T) {
 	var val byte
 	tests := []struct {
-		name    string
-		a, b, c byte
-		result  byte
+		name                     string
+		data                     byte
+		lsbPosition, msbPosition uint8
+		result                   byte
 	}{
 		{"get bottom 3 bits", 0xf2, 0, 2, 2},
 		{"get low bit", 0xf0, 0, 0, 0},
@@ -87,11 +88,11 @@ func TestExtractBits(t *testing.T) {
 	}
 	for _, tt := range tests {
 
-		val = ExtractBits(tt.a, tt.b, tt.c)
+		val = ExtractBits(tt.data, tt.lsbPosition, tt.msbPosition)
 		if val != tt.result {
 			t.Errorf("Test '%v' failed: ExtractBits(%v, %v, %v) was %v, should be %v",
 				tt.name,
-				tt.a, tt.b, tt.c,
+				tt.data, tt.lsbPosition, tt.msbPosition,
 				val,
 				tt.result)
 		}
