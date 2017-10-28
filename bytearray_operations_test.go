5e68db1cb7e45fd6a475c218bd226c8c57ff0927
Romain Lespinasse <romain.lespinasse@gmail.com>
2017-10-28 22:09:56 +0200

    [#7] support little/big endian operations on a byte array
    - Create BigEndianOperations accessor with
      - Mask function,
      - InclusiveMerge function,
      - ExclusiveMerge function,
      - ExtractBytes function.
    
    - Create LittleEndianOperations accessor with
      - Mask function,
      - InclusiveMerge function,
      - ExclusiveMerge function,
      - ExtractBytes function.
    
    - [#31] Fix LeftShift and RightShift functions.
    
    - Add some Endianess-free operations
      - Add Mask function on two arrays of the same size,
      - Add InclusiveMerge function on two arrays of the same size,
      - Add ExclusiveMerge function on two arrays of the same size,
      - Add LeftPad function,
      - Add RightPad function.
    
    Closes #7, #31

--- bytearray_operations_test.go
+++ bytearray_operations_test.go
@@ -5,6 +5,45 @@ import (
 	"testing"
 )
 
+var testcasesLeftShift = []struct {
+	name   string
+	data   []byte
+	shift  uint64
+	result []byte
+}{
+	{"no shift to the left", []byte{0xDA, 0x99, 0xBA}, 0, []byte{0xDA, 0x99, 0xBA}},
+	{"low shift to the left", []byte{0xDA, 0x99, 0xBA}, 1, []byte{0xB5, 0x33, 0x74}},
+	{"middle shift to the left", []byte{0xDA, 0x99, 0xBA}, 8, []byte{0x99, 0xBA, 0x00}},
+	{"high shift to the left", []byte{0xDA, 0x99, 0xBA}, 16, []byte{0xBA, 0x00, 0x00}},
+	{"overflow shift to the left", []byte{0xDA, 0x99, 0xBA}, 24, []byte{0x00, 0x00, 0x00}},
+}
+
+func TestLeftShift(t *testing.T) {
+	var val []byte
+	for _, tc := range testcasesLeftShift {
+		t.Run(tc.name, func(t *testing.T) {
+			val = LeftShift(tc.data, tc.shift)
+			if !reflect.DeepEqual(val, tc.result) {
+				t.Errorf("LeftShift(%x, %v) was %x, should be %x",
+					tc.data, tc.shift,
+					val,
+					tc.result)
+			}
+		})
+	}
+}
+
+func BenchmarkLeftShift(b *testing.B) {
+	var val []byte
+	for _, tc := range testcasesLeftShift {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				val = LeftShift(tc.data, tc.shift)
+			}
+		})
+	}
+}
+
 var testcasesRightShift = []struct {
 	name   string
 	data   []byte
@@ -12,9 +51,9 @@ var testcasesRightShift = []struct {
 	result []byte
 }{
 	{"no shift to the right", []byte{0xDA, 0x99, 0xBA}, 0, []byte{0xDA, 0x99, 0xBA}},
-	{"low shift to the right", []byte{0xDA, 0x99, 0xBA}, 1, []byte{0xB5, 0x33, 0x74}},
-	{"middle shift to the right", []byte{0xDA, 0x99, 0xBA}, 8, []byte{0x99, 0xBA, 0x00}},
-	{"high shift to the right", []byte{0xDA, 0x99, 0xBA}, 16, []byte{0xBA, 0x00, 0x00}},
+	{"low shift to the right", []byte{0xDA, 0x99, 0xBA}, 1, []byte{0x6D, 0x4C, 0xDD}},
+	{"middle shift to the right", []byte{0xDA, 0x99, 0xBA}, 8, []byte{0x00, 0xDA, 0x99}},
+	{"high shift to the right", []byte{0xDA, 0x99, 0xBA}, 16, []byte{0x00, 0x00, 0xDA}},
 	{"overflow shift to the right", []byte{0xDA, 0x99, 0xBA}, 24, []byte{0x00, 0x00, 0x00}},
 }
 
@@ -44,151 +83,24 @@ func BenchmarkRightShift(b *testing.B) {
 	}
 }
 
-var testcasesLeftShift = []struct {
-	name   string
-	data   []byte
-	shift  uint64
-	result []byte
-}{
-	{"no shift to the left", []byte{0xDA, 0x99, 0xBA}, 0, []byte{0xDA, 0x99, 0xBA}},
-	{"low shift to the left", []byte{0xDA, 0x99, 0xBA}, 1, []byte{0x6D, 0x4C, 0xDD}},
-	{"middle shift to the left", []byte{0xDA, 0x99, 0xBA}, 8, []byte{0x00, 0xDA, 0x99}},
-	{"high shift to the left", []byte{0xDA, 0x99, 0xBA}, 16, []byte{0x00, 0x00, 0xDA}},
-	{"overflow shift to the left", []byte{0xDA, 0x99, 0xBA}, 24, []byte{0x00, 0x00, 0x00}},
-}
-
-func TestLeftShift(t *testing.T) {
-	var val []byte
-	for _, tc := range testcasesLeftShift {
-		t.Run(tc.name, func(t *testing.T) {
-			val = LeftShift(tc.data, tc.shift)
-			if !reflect.DeepEqual(val, tc.result) {
-				t.Errorf("LeftShift(%x, %v) was %x, should be %x",
-					tc.data, tc.shift,
-					val,
-					tc.result)
-			}
-		})
+func TestMaskError(t *testing.T) {
+	val, err := Mask([]byte{0x00, 0x00}, []byte{0x00})
+	if err == nil || val != nil {
+		t.Errorf("Mask with two byte arrays of different size needs to return an error and no value")
 	}
 }
 
-var testcasesMask = []struct {
-	name   string
-	data   []byte
-	mask   []byte
-	result []byte
-}{
-	{"data and mask of equal length", []byte{0xDA, 0x99, 0xBA}, []byte{0xAD, 0x11, 0xAB}, []byte{0x88, 0x11, 0xAA}},
-	{"data shorter than mask", []byte{0xDA, 0x99, 0xBA}, []byte{0xAD, 0x11, 0xAB, 0x88, 0x11, 0xAA}, []byte{0x88, 0x11, 0xAA}},
-	{"data longer than mask", []byte{0xAD, 0x11, 0xAB, 0x88, 0x11, 0xAA}, []byte{0xDA, 0x99, 0xBA}, []byte{0xAD, 0x11, 0xAB, 0x88, 0x11, 0xAA}},
-	{"empty mask on data", []byte{0xDA, 0x99, 0xBA}, []byte{}, []byte{0xDA, 0x99, 0xBA}},
-	{"mask on empty data", []byte{}, []byte{0xAD, 0x11, 0xAB}, []byte{}},
-	{"empty mask on empty data", []byte{}, []byte{}, []byte{}},
-}
-
-func TestMask(t *testing.T) {
-	var val []byte
-	for _, tc := range testcasesMask {
-		t.Run(tc.name, func(t *testing.T) {
-			val = Mask(tc.data, tc.mask)
-			if !reflect.DeepEqual(val, tc.result) {
-				t.Errorf("Mask(%x, %x) was %x, should be %x",
-					tc.data, tc.mask,
-					val,
-					tc.result)
-			}
-		})
+func TestInclusiveMergeError(t *testing.T) {
+	val, err := InclusiveMerge([]byte{0x00, 0x00}, []byte{0x00})
+	if err == nil || val != nil {
+		t.Errorf("InclusiveMerge with two byte arrays of different size needs to return an error and no value")
 	}
 }
 
-func BenchmarkMask(b *testing.B) {
-	var val []byte
-	for _, tc := range testcasesMask {
-		b.Run(tc.name, func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				val = Mask(tc.data, tc.mask)
-			}
-		})
-	}
-}
-
-var testcasesInclusiveMerge = []struct {
-	name   string
-	data1  []byte
-	data2  []byte
-	result []byte
-}{
-	{"equal length arrays", []byte{0xDA, 0x99, 0xBA}, []byte{0xAD, 0x11, 0xAB}, []byte{0xFF, 0x99, 0xBB}},
-	{"first array longer", []byte{0xAD, 0x11, 0xAB, 0xFF, 0x99, 0xBB}, []byte{0xDA, 0x99, 0xBA}, []byte{0xAD, 0x11, 0xAB, 0xFF, 0x99, 0xBB}},
-	{"second array longer", []byte{0xDA, 0x99, 0xBA}, []byte{0xAD, 0x11, 0xAB, 0xFF, 0x99, 0xBB}, []byte{0xAD, 0x11, 0xAB, 0xFF, 0x99, 0xBB}},
-	{"first array empty", []byte{}, []byte{0xAD, 0x11, 0xAB}, []byte{0xAD, 0x11, 0xAB}},
-	{"second array empty", []byte{0xDA, 0x99, 0xBA}, []byte{}, []byte{0xDA, 0x99, 0xBA}},
-	{"empty arrays", []byte{}, []byte{}, []byte{}},
-}
-
-func TestInclusiveMerge(t *testing.T) {
-	var val []byte
-	for _, tc := range testcasesInclusiveMerge {
-		t.Run(tc.name, func(t *testing.T) {
-			val = InclusiveMerge(tc.data1, tc.data2)
-			if !reflect.DeepEqual(val, tc.result) {
-				t.Errorf("InclusiveMerge(%x, %x) was %x, should be %x",
-					tc.data1, tc.data2,
-					val,
-					tc.result)
-			}
-		})
-	}
-}
-
-func BenchmarkInclusiveMerge(b *testing.B) {
-	var val []byte
-	for _, tc := range testcasesInclusiveMerge {
-		b.Run(tc.name, func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				val = InclusiveMerge(tc.data1, tc.data2)
-			}
-		})
-	}
-}
-
-var testcasesExclusiveMerge = []struct {
-	name   string
-	data1  []byte
-	data2  []byte
-	result []byte
-}{
-	{"equal length arrays", []byte{0xDA, 0x99, 0xBA}, []byte{0xAD, 0x11, 0xAB}, []byte{0x77, 0x88, 0x11}},
-	{"first array longer", []byte{0xAD, 0x11, 0xAB, 0x77, 0x88, 0x11}, []byte{0xDA, 0x99, 0xBA}, []byte{0xAD, 0x11, 0xAB, 0xAD, 0x11, 0xAB}},
-	{"second array longer", []byte{0xAD, 0x11, 0xAB}, []byte{0xAB, 0xCD, 0xEF, 0x77, 0x88, 0x11}, []byte{0xAB, 0xCD, 0xEF, 0xDA, 0x99, 0xBA}},
-	{"first array empty", []byte{}, []byte{0xAD, 0x11, 0xAB}, []byte{0xAD, 0x11, 0xAB}},
-	{"second array empty", []byte{0xDA, 0x99, 0xBA}, []byte{}, []byte{0xDA, 0x99, 0xBA}},
-	{"empty arrays", []byte{}, []byte{}, []byte{}},
-}
-
-func TestExclusiveMerge(t *testing.T) {
-	var val []byte
-	for _, tc := range testcasesExclusiveMerge {
-		t.Run(tc.name, func(t *testing.T) {
-			val = ExclusiveMerge(tc.data1, tc.data2)
-			if !reflect.DeepEqual(val, tc.result) {
-				t.Errorf("ExclusiveMerge(%x, %x) was %x, should be %x",
-					tc.data1, tc.data2,
-					val,
-					tc.result)
-			}
-		})
-	}
-}
-
-func BenchmarkExclusiveMerge(b *testing.B) {
-	var val []byte
-	for _, tc := range testcasesExclusiveMerge {
-		b.Run(tc.name, func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				val = ExclusiveMerge(tc.data1, tc.data2)
-			}
-		})
+func TestExclusiveMergeError(t *testing.T) {
+	val, err := ExclusiveMerge([]byte{0x00, 0x00}, []byte{0x00})
+	if err == nil || val != nil {
+		t.Errorf("ExclusiveMerge with two byte arrays of different size needs to return an error and no value")
 	}
 }
 
@@ -227,42 +139,28 @@ func BenchmarkNot(b *testing.B) {
 	}
 }
 
-func BenchmarkLeftShift(b *testing.B) {
-	var val []byte
-	for _, tc := range testcasesLeftShift {
-		b.Run(tc.name, func(b *testing.B) {
-			for i := 0; i < b.N; i++ {
-				val = LeftShift(tc.data, tc.shift)
-			}
-		})
-	}
-}
-
-var testcasesExtractBytes = []struct {
-	name                     string
-	data                     []byte
-	lsbPosition, msbPosition uint64
-	result                   []byte
+var testcasesLeftPad = []struct {
+	name   string
+	data   []byte
+	length int
+	filler byte
+	result []byte
 }{
-	{"extract nothing", []byte{0xDA, 0x99, 0xBA}, 0, 0, []byte{}},
-	{"extract nothing due to inversed positions", []byte{0xDA, 0x99, 0xBA}, 16, 8, []byte{}},
-	{"extract nothing due to wrong positions", []byte{0xDA, 0x99, 0xBA}, 100, 101, []byte{}},
-	{"extract only in one byte", []byte{0xDA, 0x99, 0xBA}, 5, 7, []byte{0x05}},
-	{"extract one byte over two bytes", []byte{0xDA, 0x99, 0xBA}, 7, 8, []byte{0x03}},
-	{"extract two bytes over three bytes", []byte{0xDA, 0x99, 0xBA}, 6, 17, []byte{0x0A, 0x66}},
-	{"extract three bytes over three bytes", []byte{0xDA, 0x99, 0xBA}, 1, 22, []byte{0x2D, 0x4C, 0xDD}},
-	{"extract all bytes", []byte{0xDA, 0x99, 0xBA}, 0, 23, []byte{0xDA, 0x99, 0xBA}},
-	{"extract all bytes with an overflow position", []byte{0xDA, 0x99, 0xBA}, 0, 100, []byte{0xDA, 0x99, 0xBA}},
+	{"lpad to two elements on an smaller array", []byte{0xDA}, 2, 0x00, []byte{0x00, 0xDA}},
+	{"lpad to two elements with a filler on an smaller array", []byte{0xDA}, 2, 0x00, []byte{0x00, 0xDA}},
+	{"lpad to two elements on an empty array", []byte{}, 2, 0x11, []byte{0x11, 0x11}},
+	{"lpad to one element on an larger array", []byte{0xDA, 0x99, 0xBA}, 1, 0x11, []byte{0xDA, 0x99, 0xBA}},
+	{"lpad to negative element size change nothing", []byte{0xDA}, -1, 0x11, []byte{0xDA}},
 }
 
-func TestExtractBytes(t *testing.T) {
+func TestLeftPad(t *testing.T) {
 	var val []byte
-	for _, tc := range testcasesExtractBytes {
+	for _, tc := range testcasesLeftPad {
 		t.Run(tc.name, func(t *testing.T) {
-			val = ExtractBytes(tc.data, tc.lsbPosition, tc.msbPosition)
+			val = leftPad(tc.data, tc.length, tc.filler)
 			if !reflect.DeepEqual(val, tc.result) {
-				t.Errorf("ExtractBytes(%x, %v, %v) was %x, should be %x",
-					tc.data, tc.lsbPosition, tc.msbPosition,
+				t.Errorf("leftPad(%x) was %x, should be %x",
+					tc.data,
 					val,
 					tc.result)
 			}
@@ -270,12 +168,52 @@ func TestExtractBytes(t *testing.T) {
 	}
 }
 
-func BenchmarkExtractBytes(b *testing.B) {
+func BenchmarkLeftPad(b *testing.B) {
 	var val []byte
-	for _, tc := range testcasesExtractBytes {
+	for _, tc := range testcasesLeftPad {
 		b.Run(tc.name, func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				val = ExtractBytes(tc.data, tc.lsbPosition, tc.msbPosition)
+				val = leftPad(tc.data, tc.length, tc.filler)
+			}
+		})
+	}
+}
+
+var testcasesRightPad = []struct {
+	name   string
+	data   []byte
+	length int
+	filler byte
+	result []byte
+}{
+	{"rpad to two elements on an smaller array", []byte{0xDA}, 2, 0x00, []byte{0xDA, 0x00}},
+	{"lpad to two elements with a filler on an smaller array", []byte{0xDA}, 2, 0x00, []byte{0xDA, 0x00}},
+	{"rpad to two elements on an empty array", []byte{}, 2, 0x11, []byte{0x11, 0x11}},
+	{"rpad to one element on an larger array", []byte{0xDA, 0x99, 0xBA}, 1, 0x11, []byte{0xDA, 0x99, 0xBA}},
+	{"rpad to negative element size change nothing", []byte{0xDA}, -1, 0x00, []byte{0xDA}},
+}
+
+func TestRightPad(t *testing.T) {
+	var val []byte
+	for _, tc := range testcasesRightPad {
+		t.Run(tc.name, func(t *testing.T) {
+			val = rightPad(tc.data, tc.length, tc.filler)
+			if !reflect.DeepEqual(val, tc.result) {
+				t.Errorf("rightPad(%x) was %x, should be %x",
+					tc.data,
+					val,
+					tc.result)
+			}
+		})
+	}
+}
+
+func BenchmarkRightPad(b *testing.B) {
+	var val []byte
+	for _, tc := range testcasesRightPad {
+		b.Run(tc.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				val = rightPad(tc.data, tc.length, tc.filler)
 			}
 		})
 	}
