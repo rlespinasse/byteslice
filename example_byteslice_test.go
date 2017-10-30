0ad781e8f8b1d81c146013b2e72ed9ee865aae65
andri-de <andri.de@tnis.com>
2017-10-30 14:47:36 +0700

    add unset examle

--- example_byteslice_test.go
+++ example_byteslice_test.go
@@ -1,6 +1,9 @@
 package byteslice
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 func ExampleReverse() {
 	data := []byte{0x55, 0xDA, 0xBA}
@@ -11,7 +14,18 @@ func ExampleReverse() {
 
 func ExampleLPad() {
 	data := []byte{0x55, 0xDA, 0xBA}
-	
+
 	fmt.Printf("%x\n", LPad(data, 5, 0x22))
 	// Output: 222255daba
+}
+
+func ExampleUnset() {
+	data1 := []byte{0x00, 0x00}
+	data2 := []byte{0x00}
+	val, err := Unset(data1, data2)
+	if err == nil || val != nil {
+		log.Println("Unset with two byte slices of different size needs to return an error and no value")
+	}
+	fmt.Printf("%x\n", val)
+	// Output: []
 }
