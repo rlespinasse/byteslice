97bd93e9236cd7ccc9d2709aafbe1e82b546a89c
Christopher Hersh <Chris@ChrisHersh.com>
2017-10-30 02:33:18 -0400

    [#35] Add documentation example for LPad (#48)
    Closes #35

--- example_byteslice_test.go
+++ example_byteslice_test.go
@@ -8,3 +8,10 @@ func ExampleReverse() {
 	fmt.Printf("%x\n", Reverse(data))
 	// Output: bada55
 }
+
+func ExampleLPad() {
+	data := []byte{0x55, 0xDA, 0xBA}
+	
+	fmt.Printf("%x\n", LPad(data, 5, 0x22))
+	// Output: 222255daba
+}
