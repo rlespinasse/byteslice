8ca0019d3f5f9b017759f2c98d7daec89a88c399
Andrei Simionescu <ansimionescu@gmail.com>
2017-10-31 11:25:18 +0200

    Add ExampleLToogle, fix #38

--- example_byteslice_bigendian_test.go
+++ example_byteslice_bigendian_test.go
@@ -9,3 +9,11 @@ func ExampleLSet() {
 	fmt.Printf("%x\n", LSet(data, setData))
 	// Output: bada55
 }
+
+func ExampleLToogle() {
+	data := []byte{0xAB, 0xCB, 0x44}
+	setData := []byte{0x11, 0x11, 0x11}
+
+	fmt.Printf("%x\n", LToogle(data, setData))
+	// Output: bada55
+}
