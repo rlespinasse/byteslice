478136d0ce9cc044161cd0310084b3bfdc60d98a
Andrei Simionescu <ansimionescu@gmail.com>
2017-10-31 11:21:31 +0200

    Add ExampleLUnset, fix #39

--- example_byteslice_bigendian_test.go
+++ example_byteslice_bigendian_test.go
@@ -9,3 +9,11 @@ func ExampleLSet() {
 	fmt.Printf("%x\n", LSet(data, setData))
 	// Output: bada55
 }
+
+func ExampleLUnset() {
+	data := []byte{0xAA, 0xCA, 0x55}
+	setData := []byte{0x10, 0x12}
+
+	fmt.Printf("%x\n", LUnset(data, setData))
+	// Output: 000255
+}
