cb68ae198091a44147a465aed56a2ddd57c490a9
George Kelly <gdog.kelly@googlemail.com>
2017-10-31 08:50:25 +0000

    [#46] Added examples for Toggle
    One 'simple' example which uses 000 and 1011
    
    One regular example with the output bada55

--- example_byteslice_test.go
+++ example_byteslice_test.go
@@ -11,7 +11,25 @@ func ExampleReverse() {
 
 func ExampleLPad() {
 	data := []byte{0x55, 0xDA, 0xBA}
-	
+
 	fmt.Printf("%x\n", LPad(data, 5, 0x22))
 	// Output: 222255daba
+}
+
+func ExampleToggle() {
+	data := []byte{0xbb, 0xdb, 0x54}
+	toggle := []byte{0x01, 0x01, 0x01}
+
+	output, _ := Toggle(data, toggle)
+	fmt.Printf("%x\n", output)
+	// Output: bada55
+}
+
+func ExampleToggle_simple() {
+	data := []byte{0x00, 0x01, 0x00}
+	toggle := []byte{0x01, 0x00, 0x01}
+
+	output, _ := Toggle(data, toggle)
+	fmt.Printf("%x\n", output)
+	// Output: 010101
 }
