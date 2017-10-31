e74fa29832c72f1fa98542aa489a7ffa588c4ebe
Andrei Simionescu <ansimionescu@gmail.com>
2017-10-31 11:01:11 +0200

    Fixes #47

--- example_byteslice_test.go
+++ example_byteslice_test.go
@@ -11,7 +11,16 @@ func ExampleReverse() {
 
 func ExampleLPad() {
 	data := []byte{0x55, 0xDA, 0xBA}
-	
+
 	fmt.Printf("%x\n", LPad(data, 5, 0x22))
 	// Output: 222255daba
+}
+
+func ExampleUnset() {
+	data := []byte{0xDE, 0xAD, 0xBE, 0xEF}
+	zeroes := []byte{0x00, 0x00, 0x00, 0x00}
+
+	dat, err := Unset(data, zeroes)
+	fmt.Println(err, dat)
+	// Output: <nil> [0 0 0 0]
 }
