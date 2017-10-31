2804b56df00011f14c3ab717274af64d6843d53b
Andrei Simionescu <ansimionescu@gmail.com>
2017-10-31 11:09:29 +0200

    Add Flip example, fix #34

--- example_byteslice_test.go
+++ example_byteslice_test.go
@@ -2,6 +2,13 @@ package byteslice
 
 import "fmt"
 
+func ExampleFlip() {
+	data := []byte{0x55, 0xDA, 0xBA}
+
+	fmt.Printf("%x\n", Flip(data))
+	// Output: aa2545
+}
+
 func ExampleReverse() {
 	data := []byte{0x55, 0xDA, 0xBA}
 
@@ -11,7 +18,7 @@ func ExampleReverse() {
 
 func ExampleLPad() {
 	data := []byte{0x55, 0xDA, 0xBA}
-	
+
 	fmt.Printf("%x\n", LPad(data, 5, 0x22))
 	// Output: 222255daba
 }
