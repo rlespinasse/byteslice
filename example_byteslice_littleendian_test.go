3e6b4490dc57516cada293b4df21bf7436996d46
Romain Lespinasse <romain.lespinasse@gmail.com>
2018-10-06 22:35:53 +0200

    docs(byteslice_littleendian): add runset example

--- example_byteslice_littleendian_test.go
+++ example_byteslice_littleendian_test.go
@@ -9,3 +9,12 @@ func ExampleRSet() {
 	fmt.Printf("%x\n", RSet(data, setData))
 	// Output: bada55
 }
+
+func ExampleRUnset() {
+	data := []byte{0x11, 0x11, 0x00}
+	unsetData := []byte{0x01, 0x01}
+
+	output := RUnset(data, unsetData)
+	fmt.Printf("%x\n", output)
+	// Output: 110100
+}
